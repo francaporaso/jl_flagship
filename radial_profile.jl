ad7241b8a28fb780df32959973d9372b4f1228e8
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-08-22 16:34:29 -0300

    update, falta guardado

--- radial_profile.jl
+++ radial_profile.jl
@@ -43,7 +43,8 @@ function get_tracers(RMAX, rv::Vector{Float64}, xv::Vector{Float64}, yv::Vector{
         sq_distance = @views @. (tcat[:,4] - xv[v])^2 + (tcat[:,5] - yv[v])^2 + (tcat[:,6] - zv[v])^2
         mask = sq_distance .<= (RMAX*rv[v])^2
 
-        trac_lists[v] = tcat[mask,:]
+        trac_lists[v] = hcat(tcat[mask,:], sq_distance[mask])
+        trac_lists[v] = trac_lists[v][sortperm(trac_lists[v][:,end]),:]
     end
 
     return trac_lists
@@ -59,19 +60,20 @@ function partial_profile(tcat::Matrix{Float64},
     RMIN *= rv
     RMAX *= rv
     dr   *= rv
-    sq_distance = @. (tcat[:,4] - xv)^2 + (tcat[:,5] - yv)^2 + (tcat[:,6] - zv)^2
+    #sq_distance = @. (tcat[:,4] - xv)^2 + (tcat[:,5] - yv)^2 + (tcat[:,6] - zv)^2
 
     Delta   = zeros(Float64, NBINS)
     DenTrac = zeros(Float64, NBINS)
     rad = RMIN
 
     for i in 1:NBINS
-        tr = @views tcat[(sq_distance .>= rad^2) .& (sq_distance .< (rad+dr)^2), :]
+        #tr = @views tcat[(sq_distance .>= rad^2) .& (sq_distance .< (rad+dr)^2), :]
+        tr = @views tcat[(tcat[:,end] .< (rad+dr)^2) .&& (tcat[:,end] .>= rad^2), :]
         mass   = sum(10.0 .^ tr[:,3])
         volume = (4.0 *pi/ 3.0 )*((rad+dr)^3 - rad^3)
         
         Delta[i]   = (mass/volume)/MeanDenTrac - 1.0
-        DenTrac[i] = length(tr[:,1])/volume
+        DenTrac[i] = length(tr[:,1])#/volume
 
         rad += dr
     end
@@ -84,9 +86,9 @@ function radial_profile(RMIN, RMAX, dr,
                         Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max,
                         flag, lensname::String, tracname::String)
     ## reading cats
-    println("Reading catalogs...")
-
+    println("Reading lenses...")
     L = lenscat_load(Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag=flag, lensname=lensname)
+    println("Reading tracers...")
     tracers_lists = get_tracers(RMAX, L[:,2], L[:,6], L[:,7], L[:,8], tracname=tracname)
     
     Nvoids = length(L[:,1])
@@ -110,32 +112,24 @@ function radial_profile(RMIN, RMAX, dr,
     Dentrac_mean = mean(DenTrac, dims=1)'
     Dentrac_std  = std(DenTrac, dims=1)'
 
-    println("Plotting...")
-    r = range(RMIN, RMAX, NBINS)
-    p1 = plot(legend=false)
-    p2 = plot(legend=false)
-    # for i in 1:Nvoids
-    #     plot!(p1, r, Delta[i,:], lc=:blue, alpha=0.5)
-    #     plot!(p2, r, DenTrac[i,:], lc=:red, alpha=0.5) 
-    # end
+    println("Saving...")
 
-    plot!(p1, r, Delta_mean, yerr=Delta_std, lc=:darkblue)
-    plot!(p2, r, Dentrac_mean, yerr=Dentrac_std, lc=:darkred)
+    writedlm("pru.csv", Delta, ',')
 
-    png(p1, "prof1_pru")
-    png(p2, "prof2_pru")
     println("End!")
 end
 
 ## -- main --
 function main()
     RMIN, RMAX, dr = 0.01, 5., 0.1
-    Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag = 5., 15., 0.2, 0.3, -1., -0.9, -1., 100., 2
+    Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag = 10., 12., 0.2, 0.25, -1., -0.9, -1., 100., 2
     lensname = "/home/franco/FAMAF/Lensing/cats/MICE/voids_MICE.dat"
     tracname = "/home/franco/FAMAF/Lensing/cats/MICE/mice-halos-cut.fits"
     # lensname = "/mnt/simulations/MICE/voids_MICE.dat"
     # tracname = "/home/fcaporaso/cats/MICE/micecat2_halos_full.fits"
+
+
     radial_profile(RMIN, RMAX, dr, Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag, lensname, tracname)
 end
 
-main()
+#main()
