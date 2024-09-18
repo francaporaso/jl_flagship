80dd3a49e0de45b4a887fc51dedc12856491fe05
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-09-18 10:53:12 +0200

    udpate

--- density_test.jl
+++ density_test.jl
@@ -25,7 +25,7 @@ function test()
     # leyendo cat de lentes
     println("leyendo voids")
     L = lenscat_load(Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, lensname=lensname)
-    println("nvoids: $nrow(L)")
+    println("nvoids:", nrow(L))
     # leyendo halos al rededro de xv, yv, zv hasta RMAX
     println("leyendo halos")
     tr = get_tracers(RMAX, NBINS, L[!,2], L[!,6], L[!,7], L[!,8], tracname=tracname)
@@ -63,8 +63,10 @@ function plot_result(voidid, ρ, ρ_universe)
 end
 
 ### ---------------------------------------------- main
-test()
-
+t = @elapsed begin
+    test()
+end
+println("terminado en $t sec")
 pp = false
 
 if pp
