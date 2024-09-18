c3118d5244057ec4199056769907d2972b362566
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-09-18 09:50:45 +0200

    update, ready to run in mirtha

--- density_test.jl
+++ density_test.jl
@@ -13,15 +13,15 @@ function mean_density_comovilshell(logm, rv, RMAX)
 end
 
 
-### ----------------- main
-# DR = (RMAX)/NBINS
-
-const pp = true
+### ---------------------------------------------- main
+const pp = false
 
 const RMIN, RMAX, NBINS = 0.05, 5., 100
 const Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag = 10., 12., 0.2, 0.25, -1., -0.9, -1.0, 100.0, 2
 const lensname = "/home/franco/FAMAF/Lensing/cats/MICE/voids_MICE.dat"
 const tracname = "/home/franco/FAMAF/Lensing/cats/MICE/mice-halos-cut.fits"
+# const lensname = "/mnt/simulations/MICE/voids_MICE.dat"
+# const tracname = "/home/fcaporaso/cats/MICE/micecat2_halos_full.fits"
 
 # leyendo cat de lentes
 L = lenscat_load(Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max)
@@ -30,27 +30,26 @@ nvoids = nrow(L)
 tr = get_tracers(RMAX, NBINS, L[!,2], L[!,6], L[!,7], L[!,8])
 
 # calculando la densidad media de cada void en caja
-md = zeros(nvoids)
+ρ = zeros(nvoids)
 for i in 1:nvoids
-    md[i] = mean_density_box(tr[i][:,1], L[i,2], RMAX)
+    ρ[i] = mean_density_box(tr[i][:,1], L[i,2], RMAX)
 end
-
-open("den_box_test.csv", "w") do io
-    writedlm(io,  md, ',')
-end
-
 
 # densidad media teorica
-md_universe = mean_density.(L[!,5], H0, Om0, Ode0)
+ρ_universe = mean_density.(L[!,5], H0, Om0, Ode0)
+
+open("den_box_test.csv", "w") do io
+    writedlm(io,  [ρ ρ_universe], ',')
+end
 
 println("END!")
 
-using Plots
 if pp
+    using Plots
     p = plot(legend=:outertopright)
     # plot!(p, L[!, 1], md, label="Mean density box")
     # plot!(p, L[!, 1], md_universe, label="Mean density universe")
-    plot!(p, L[!,1], md./md_universe, label="\\rho_{box} / \\rho_{uni} ")
+    plot!(p, L[!,1], ρ ./ ρ_universe, label="\\rho_{box} / \\rho_{uni} ")
     plot!(yscale=:log10)
     display(p)
 end
