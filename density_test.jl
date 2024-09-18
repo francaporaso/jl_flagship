882de79a0f7264d12fb31501d2db0f37a2b27053
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-09-18 09:51:28 +0200

    update, ready to run in mirtha

--- density_test.jl
+++ density_test.jl
@@ -18,10 +18,10 @@ const pp = false
 
 const RMIN, RMAX, NBINS = 0.05, 5., 100
 const Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag = 10., 12., 0.2, 0.25, -1., -0.9, -1.0, 100.0, 2
-const lensname = "/home/franco/FAMAF/Lensing/cats/MICE/voids_MICE.dat"
-const tracname = "/home/franco/FAMAF/Lensing/cats/MICE/mice-halos-cut.fits"
-# const lensname = "/mnt/simulations/MICE/voids_MICE.dat"
-# const tracname = "/home/fcaporaso/cats/MICE/micecat2_halos_full.fits"
+# const lensname = "/home/franco/FAMAF/Lensing/cats/MICE/voids_MICE.dat"
+# const tracname = "/home/franco/FAMAF/Lensing/cats/MICE/mice-halos-cut.fits"
+const lensname = "/mnt/simulations/MICE/voids_MICE.dat"
+const tracname = "/home/fcaporaso/cats/MICE/micecat2_halos_full.fits"
 
 # leyendo cat de lentes
 L = lenscat_load(Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max)
