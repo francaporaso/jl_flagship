1932784fe2a26978b9fde7aecf25950fcfbfed80
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-09-19 12:38:31 +0200

    update, setting params to Float32

--- main.jl
+++ main.jl
@@ -3,12 +3,12 @@ include("radial_profile.jl")
 
 # ------------------------------------------------ #
 #                                             main #
-const RMIN, RMAX, NBINS = 0.5, 4., 100
-const Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag = 10., 12., 0.2, 0.25, -1., -0.9, -1.0, 100.0, 2
-const lensname = "/home/franco/FAMAF/Lensing/cats/MICE/voids_MICE.dat"
-const tracname = "/home/franco/FAMAF/Lensing/cats/MICE/mice-halos-cut.fits"
-# const lensname = "/mnt/simulations/MICE/voids_MICE.dat"
-# const tracname = "/home/fcaporaso/cats/MICE/mice_halos_centralesF.fits"
+const RMIN, RMAX, NBINS = 0.05f0, 5.0f0, Int32(100)
+const Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag = 10.0f0, 12.0f0, 0.2f0, 0.25f0, -1.0f0, -0.9f0, -1.0f0, 100.0f0, 2
+# const lensname = "/home/franco/FAMAF/Lensing/cats/MICE/voids_MICE.dat"
+# const tracname = "/home/franco/FAMAF/Lensing/cats/MICE/mice-halos-cut.fits"
+const lensname = "/mnt/simulations/MICE/voids_MICE.dat"
+const tracname = "/home/fcaporaso/cats/MICE/mice_halos_centralesF.fits"
 
 # const NTRACS = length(read(FITS(tracname)[2], "unique_gal_id"))
 const NTRACS = length(read(FITS(tracname)[2], "unique_halo_id"))
