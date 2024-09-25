5b391de70002fe20bde0c01e3918262b2ff3ba88
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-09-25 11:05:31 +0200

    update stacking

--- main.jl
+++ main.jl
@@ -5,15 +5,13 @@ include("radial_profile.jl")
 #                                             main #
 const RMIN, RMAX, NBINS = 0.05f0, 5.0f0, Int32(100)
 const Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag = 10.0f0, 15.0f0, 0.2f0, 0.25f0, -1.0f0, -0.9f0, 0.1f0, 100.0f0, 2
-# const lensname = "/home/franco/FAMAF/Lensing/cats/MICE/voids_MICE.dat"
-# const tracname = "/home/franco/FAMAF/Lensing/cats/MICE/mice-halos-cut.fits"
-const lensname = "/mnt/simulations/MICE/voids_MICE.dat"
-const tracname = "/home/fcaporaso/cats/MICE/mice_halos_centralesF.fits"
+const lensname = "/home/franco/FAMAF/Lensing/cats/MICE/voids_MICE.dat"
+const tracname = "/home/franco/FAMAF/Lensing/cats/MICE/mice-halos-cut.fits"
+# const lensname = "/mnt/simulations/MICE/voids_MICE.dat"
+# const tracname = "/home/fcaporaso/cats/MICE/mice_halos_centralesF.fits"
 
 t = @elapsed begin
-
-    # radial_profile(RMIN, RMAX, NBINS, Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag, lensname, tracname)
-    test_profile(RMIN, RMAX, NBINS, Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag, lensname, tracname)
+    radial_profile(RMIN, RMAX, NBINS, Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag, lensname, tracname)
 end
 
 t /= 60.0
