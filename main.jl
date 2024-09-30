a1111120bb577a8603a55fedbb3a37924860aa6c
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-09-30 14:22:12 +0200

    calculation of all individual profiles

--- main.jl
+++ main.jl
@@ -4,14 +4,15 @@ include("radial_profile.jl")
 # ------------------------------------------------ #
 #                                             main #
 const RMIN, RMAX, NBINS = 0.05f0, 5.0f0, Int32(100)
-const Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag = 10.0f0, 15.0f0, 0.2f0, 0.25f0, -1.0f0, -0.9f0, 0.1f0, 100.0f0, 2
+const Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag = 0.0, 50.0, 0.1, 0.4, -1.0f0, -0.9f0, -1.0, 100.0f0, 2
 # const lensname = "/home/franco/FAMAF/Lensing/cats/MICE/voids_MICE.dat"
 # const tracname = "/home/franco/FAMAF/Lensing/cats/MICE/mice_halos_cut.fits"
 const lensname = "/mnt/simulations/MICE/voids_MICE.dat"
 const tracname = "/home/fcaporaso/cats/MICE/mice_halos_centralesF.fits"
 
 t = @elapsed begin
-    radial_profile(RMIN, RMAX, NBINS, Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag, lensname, tracname)
+    # radial_profile(RMIN, RMAX, NBINS, Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag, lensname, tracname)
+    calculate_all_indiv(RMIN, RMAX, NBINS, Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag, lensname, tracname)
 end
 
 t /= 60.0
