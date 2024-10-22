3e42c89ec2f2fa93e01b80fca6f86746cb43886b
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-10-22 16:04:17 +0200

    mejoras en performance, dirijidas a menor consumo de memoria (o eso intento)

--- PERFILES.jl
+++ PERFILES.jl
@@ -17,31 +17,6 @@ filename = @sprintf "radialprof_stack_R_%.0f_%.0f_z%.1f_%.1f.csv" Rv_min Rv_max 
 end
 
 @everywhere begin
-    """
-    Divides a array in n/step parts. 
-    If n%step = 0 then each part has the same number of elements,
-    if not the last element in the resulting array has the rest
-    """
-    function array_split(ary, step)
-        n = size(ary)[1]
-        lbins = round(Int64, n/step)
-        if n % step != 0
-            lbins += 1
-        end
-
-        sub_ary = Vector{Matrix{Float64}}(undef, lbins)
-        x = 1
-        for i in 1:lbins
-            try
-                sub_ary[i] = ary[x:x+step-1,:]
-                x += step
-            catch e
-                sub_ary[i] = ary[x:end,:]
-            end
-        end
-
-        return sub_ary
-    end 
 
     """
     Total mass in a ball centered in rv and radius RMAX, in [Msun / h]
@@ -56,75 +31,33 @@ end
     Dado un sólo centro (xv,yv,zv) y su radio rv, encuentra los halos
     al rededor del centro entre RMIN*rv hasta RMAX*rv
     """
-    function get_halos(S::Matrix{Float32},
+    function get_halos(S,
                     RMIN, RMAX,
                     rv, xv, yv, zv)
 
         ### Máscara en una bola con centro (xv,yv,zv) y radio (1+2DR)RMAX*rv
-        distance = @views @. sqrt((S[:,1] - xv)^2 + (S[:,2] - yv)^2 + (S[:,3] - zv)^2)/rv
-        m1 = distance .<= RMAX
-        m2 = distance .>= RMIN
+        distance = @. sqrt((S[:,1] - xv)^2 + (S[:,2] - yv)^2 + (S[:,3] - zv)^2)/rv
+        m1 = @. (distance <= RMAX) && (distance >= RMIN)
 
-        halos_list = hcat(S[m1 .&& m2, end], distance[m1 .&& m2])
+        halos_list = [S[m, end] distance[m]]
 
         return halos_list
     end
 
     """
-    Loads the lenses catalog
-    """
-    function lenscat_load(Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max; 
-                        flag=2.0, lensname="/mnt/simulations/MICE/voids_MICE.dat")
-        ## L[1] = id
-        ## L[2] = rv
-        ## L[5] = z
-        ## L[6] = xv
-        ## L[7] = yv
-        ## L[8] = zv
-        ## L[9] = rho1
-        ## L[10] = rho2
-        ## L[12] = flag
-        L = readdlm(lensname, Float32)
-
-        m_rv   = @views @. (L[:,2] >= Rv_min) && (L[:,2] <= Rv_max)
-        m_z    = @views @. (L[:,5] >= z_min) && (L[:,5] <= z_max)
-        m_rho  = @views @. (L[:,9] >= rho1_min) && (L[:,9] <= rho1_max) && (L[:,10] >= rho2_min) && (L[:,10] <= rho2_max)
-        m_flag = @views @. (L[:,12] >= flag) 
-
-        mask = @views @. m_rv && m_z && m_rho && m_flag
-
-        return L[mask,:]
-    end
-
-    """
     Loads the tracers catalog
     """
-    function traccat_load(; lmhalo_min = nothing,              
-                        tracname="/home/fcaporaso/cats/MICE/mice_halos_centralesF.fits")
+    function traccat_load(; tracname::String="/home/fcaporaso/cats/MICE/mice_halos_centralesF.fits")
 
         f = FITS(tracname)[2]
-        S = Matrix{Float32}([read(f, "xhalo") read(f, "yhalo") read(f, "zhalo") read(f, "lmhalo")]) #read(f, "flag_central")])
-
-        if !isnothing(lmhalo_min)
-            return S
-        end
+        S::Matrix{Float32} = Matrix([read(f, "xhalo") read(f, "yhalo") read(f, "zhalo") read(f, "lmhalo")]) #read(f, "flag_central")])
 
         ## halos más grandes q 10 DM particles
-        mp = 2.93e10 #Msun/h
-        m_logm = @views @. S[:,end] > log10(10*mp)
+        mp = 2.93f10 #Msun/h
+        m_logm = view(S,:,4) .> log10(10*mp)
         
         return S[m_logm, :]
-
-        ### Filtros en flag_central y redshift:
-        # S = Matrix{Float32}([read(f, "z_cgal") read(f, "xhalo") read(f, "yhalo") read(f, "zhalo") read(f, "lmhalo")]) #read(f, "flag_central")])
-        # m_z = @. (S[:,1] >= (z_min-0.2)) && (S[:,1] <= (z_max+0.2))
-        # m_flag = @. (S[:,end] == zero(Float32)) ## halos centrales
-        # mask = @views @. m_z && m_flag
-        # return S[mask,2:5]    
-
-        # return S[m_z,2:end]
     end
-
 
     """ 
     Perfil parcial, masa en el void y masa en una bola de 2RMAX de 1 void.
@@ -136,11 +69,17 @@ end
                             
         #"/home/franco/FAMAF/Lensing/cats/MICE/mice_halos_cut.fits"
 
+        ## TODO idea: 
+        ### como get_halos se llama dos veces primero para tcat y dsp en mass_ball,
+        ### cambiar a que llame una unica vez con los halos necesarios para mass_ball y después descartar
+        ### con una máscara los halos que no sirven para el perfil. 
+        #### Segun @btime get_halos es PESADA de calcular, si reduzco una vez la cantidad de llamados, mejora el uso de memoria
+
         ### tcat[:,1] = logm
         ### tcat[:,2] = comovil_dist from center (xv,yv,zv) in units of void radius [rv]
-        S = traccat_load(lmhalo_min=1, tracname=tracname)
-        tcat = get_halos(S, RMIN, RMAX, rv, xv, yv, zv)
-        MassBall, HalosBall = mass_ball(S, 5RMAX, rv, xv, yv, zv)
+        S = traccat_load(tracname=tracname)
+        tcat = get_halos(view(S,:,:), RMIN, RMAX, rv, xv, yv, zv)
+        MassBall, HalosBall = mass_ball(view(S,:,:), 5RMAX, rv, xv, yv, zv)
         # MassShell, VolShell = mass_comovingshell(S, RMAX, rv, xv, yv, zv)
 
         NHalos = zeros(NBINS)
@@ -163,6 +102,28 @@ end
 end ## fin de @everywhere begin
 
 """
+Loads the lenses catalog
+"""
+function lenscat_load(Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max; 
+                    flag=2.0f0, lensname="/mnt/simulations/MICE/voids_MICE.dat")
+    ## L[1] = id
+    ## L[2] = rv
+    ## L[5] = z
+    ## L[6] = xv
+    ## L[7] = yv
+    ## L[8] = zv
+    ## L[9] = rho1
+    ## L[10] = rho2
+    ## L[12] = flag
+    L::Matrix{Float32} = Matrix(readdlm(lensname, Float32))
+
+    mask = @. (L[:,2] >= Rv_min) && (L[:,2] <= Rv_max) && (L[:,5] >= z_min) && (L[:,5] <= z_max) && (L[:,9] >= rho1_min) && (L[:,9] <= rho1_max) && 
+              (L[:,10] >= rho2_min) && (L[:,10] <= rho2_max) && (L[:,12] >= flag) 
+
+    return L[mask,:]
+end
+
+"""
 Paralelizado de partial_profile
 """
 function paralellization(partial, NCORES,
@@ -174,7 +135,7 @@ function paralellization(partial, NCORES,
     nvoids = size(L)[1]
     println("NVOIDS: .... $nvoids")
 
-    resmap = pmap(partial, fill(RMIN,nvoids), fill(RMAX,nvoids), fill(NBINS,nvoids), L[:,2], L[:,6], L[:,7], L[:,8], batch_size=NCORES)
+    resmap = pmap(partial, fill(RMIN,nvoids), fill(RMAX,nvoids), fill(NBINS,nvoids), view(L,:,2), view(L,:,6), view(L,:,7), view(L,:,8), batch_size=NCORES)
 
     return resmap
 end
