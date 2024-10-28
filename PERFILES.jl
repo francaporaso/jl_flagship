bc2baf7bef92afab6d280e00fd29827e16bed680
Franco Caporaso <fmcaporaso@mi.unc.edu.ar>
2024-10-28 16:32:16 +0100

    update

--- PERFILES.jl
+++ PERFILES.jl
@@ -4,10 +4,6 @@ using ProgressMeter
 using FITSIO, DelimitedFiles
 
 
-# NCORES = Int32(10)
-# addprocs(NCORES)
-NCORES = nprocs()
-
 RMIN, RMAX, NBINS = 0.0f0, 5.0f0, Int32(50)
 Rv_min, Rv_max, z_min, z_max, rho1_min, rho1_max, rho2_min, rho2_max, flag = 6.0f0, 9.622f0, 0.2f0, 0.4f0, -1.0f0, -0.8f0, -1.0f0, 100.0f0, 2.0f0
 filename = @sprintf "radialprof_R_%.0f_%.0f_z%.1f_%.1f_vjl.csv" Rv_min Rv_max z_min z_max
